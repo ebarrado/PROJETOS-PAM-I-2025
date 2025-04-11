a3ad26ce797f9221bc1049225237ca7e02a02dfc
ebarrado <erika_barrado@hotmail.com>
2025-04-11 09:17:58 -0300

    CRIANDO BOTÃO ELEVATEDBUTTON

--- exemplos_aula/turma_a/projeto_layout_column/lib/main.dart
+++ exemplos_aula/turma_a/projeto_layout_column/lib/main.dart
@@ -29,27 +29,33 @@ class HomeColunas extends StatelessWidget {
         backgroundColor: Colors.amber,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(40.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text("Faça Login"),
-            SizedBox(height: 10),
+            SizedBox(height: 20),
             TextFormField(
               decoration: InputDecoration(
                 labelText: "Usuário",
                 border: OutlineInputBorder(),
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 50),
             TextFormField(
               decoration: InputDecoration(
                 labelText: "Senha",
                 border: OutlineInputBorder(),
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                print("Botao Pressionado");
+              },
+              child: Text("REALIZAR LOGIN"),
+            ),
           ],
         ),
       ),

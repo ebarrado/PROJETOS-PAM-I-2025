830ff811ff3c705feb99f37affbce644019fd084
ebarrado <erika_barrado@hotmail.com>
2025-05-12 08:34:45 -0300

    Usando SingleChildScrollView e corrigindo AppBar e BottomNavigatorBar

--- correcao_atividades/turma_b/projeto_columns_rows/lib/main.dart
+++ correcao_atividades/turma_b/projeto_columns_rows/lib/main.dart
@@ -26,145 +26,220 @@ class HomeBolos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Exemplo Column e Row')),
-      body: Column(
-        children: [
-          Text(
-            'Bolo de Chocolate',
-            style: TextStyle(fontSize: 30, color: Colors.brown[900]),
+      appBar: AppBar(
+        title: const Text(
+          'Cardápio de Bolos',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+            fontFamily: 'Roboto',
+            decorationStyle: TextDecorationStyle.wavy,
           ),
-          Text(
-            "Delicioso bolo de chocolate feito com massa fofinha e cacau premium, coberto com uma generosa camada de ganache cremosa. Uma sobremesa perfeita para os amantes de chocolate!",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.brown[900],
-              fontFamily: 'Roboto',
-              decorationStyle: TextDecorationStyle.wavy,
-            ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            // Ação do botão de voltar
+          },
+        ),
+        backgroundColor: const Color.fromARGB(255, 16, 198, 204),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.white),
+            onPressed: () {
+              // Ação do botão de pesquisa
+            },
           ),
-          Image.network(
-            'https://moinhoglobo.com.br/wp-content/uploads/2019/03/08-bolo-chocolate-1024x683.png',
-            width: 400,
-            height: 400,
+          IconButton(
+            icon: const Icon(Icons.favorite, color: Colors.white),
+            onPressed: () {
+              // Ação do botão de favoritos
+            },
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
             children: [
-              Icon(Icons.star, color: Colors.yellow[700]),
-              Icon(Icons.star, color: Colors.yellow[700]),
-              Icon(Icons.star, color: Colors.yellow[700]),
-              Icon(Icons.star, color: Colors.yellow[700]),
-              Icon(Icons.star, color: Colors.yellow[700]),
+              Text(
+                'Bolo de Chocolate',
+                style: TextStyle(fontSize: 30, color: Colors.brown[900]),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                color: Colors.brown[100],
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Delicioso bolo de chocolate feito com massa fofinha e cacau premium, coberto com uma generosa camada de ganache cremosa. Uma sobremesa perfeita para os amantes de chocolate!",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.brown[900],
+                    fontFamily: 'Roboto',
+                    decorationStyle: TextDecorationStyle.solid,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+
+              Image.network(
+                'https://moinhoglobo.com.br/wp-content/uploads/2019/03/08-bolo-chocolate-1024x683.png',
+                width: 400,
+                height: 400,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(Icons.star, color: Colors.yellow[700]),
+                  Icon(Icons.star, color: Colors.yellow[700]),
+                  Icon(Icons.star, color: Colors.yellow[700]),
+                  Icon(Icons.star, color: Colors.yellow[700]),
+                  Icon(Icons.star, color: Colors.yellow[700]),
+
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    transformAlignment: Alignment.centerRight,
+                    child: Text(
+                      '4.5',
+                      style: TextStyle(fontSize: 20, color: Colors.brown[900]),
+                    ),
+                  ),
+                ],
+              ),
 
               Container(
-                padding: const EdgeInsets.all(8.0),
-                transformAlignment: Alignment.centerRight,
-                child: Text(
-                  '4.5',
-                  style: TextStyle(fontSize: 20, color: Colors.brown[900]),
+                color: Colors.brown[100],
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.access_time, color: Colors.brown[900]),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'Tempo de Preparo',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.brown[900],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              '30 min',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.brown[900],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.restaurant, color: Colors.brown[900]),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'Rendimento',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.brown[900],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              '8 porções',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.brown[900],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.star, color: Colors.brown[900]),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'Avaliação',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.brown[900],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              '4.5',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.brown[900],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-          Container(
-            color: Colors.brown[100],
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.access_time, color: Colors.brown[900]),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          'Tempo de Preparo',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.brown[900],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          '30 min',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.brown[900],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.restaurant, color: Colors.brown[900]),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          'Rendimento',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.brown[900],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          '8 porções',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.brown[900],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Icon(Icons.star, color: Colors.brown[900])]),
-                    Column(
-                      children: [
-                        Text(
-                          'Avaliação',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.brown[900],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          '4.5',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.brown[900],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: const Color.fromARGB(255, 16, 198, 204),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.home, color: Colors.white),
+              onPressed: () {
+                // Ação do botão Home
+              },
             ),
-          ),
-        ],
+            IconButton(
+              icon: const Icon(Icons.favorite, color: Colors.white),
+              onPressed: () {
+                // Ação do botão Favoritos
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.settings, color: Colors.white),
+              onPressed: () {
+                // Ação do botão Configurações
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
